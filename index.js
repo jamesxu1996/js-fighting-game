9313e86166fa854f676922135b22e43b102a20b0
James Xu <jamesxu_1996@hotmail.com>
2022-06-08 21:06:41 +1000

    created player and enemy objects

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
-// select canvas element
-const canvas = document.querySelector('canvas')
+// Select canvas element
+const canvas = document.querySelector('canvas');
 
-// set the context c as 2d
+// Set the context c as 2d
 const c = canvas.getContext('2d');
 
-// set canvas dimensions
+// Set canvas dimensions
 canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+// Set global gravity const
+const gravity = 0.2
+
+// Set Sprite class
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
+    }
+}
+
+// Create new player & enemy object using Sprite class blueprint
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+// Set animate function 
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
