176cf66a302e2147ee5e3a9df24f014b957d4933
James Xu <jamesxu_1996@hotmail.com>
2022-06-10 02:31:18 +1000

    implemented player idle animation

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ const player = new Fighter({
 		x: 0,
 		y: 0,
 	},
+    imageSrc: "./assets/samuraiMack/Idle.png",
+    framesMax: 8,
+    scale: 2.5,
+    offset: {
+        x: 215,
+        y: 156
+    }
 });
 
 const enemy = new Fighter({
@@ -91,7 +98,7 @@ function animate() {
 	background.update();
 	shop.update();
 	player.update();
-	enemy.update();
+	// enemy.update();
 
 	// Set default velocity for player and enemy at 0
 	player.velocity.x = 0;
--- js/classes.js
+++ js/classes.js
@@ -1,50 +1,75 @@
 // Set Sprite class
 class Sprite {
-	constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
+	constructor({
+		position,
+		imageSrc,
+		scale = 1,
+		framesMax = 1,
+		offset = {x: 0, y: 0}
+	}) {
 		this.position = position;
 		this.width = 50;
 		this.height = 150;
 		this.image = new Image();
 		this.image.src = imageSrc;
 		this.scale = scale;
-        this.framesMax = framesMax
-        this.framesCurrent = 0
-        this.framesElapsed = 0
-        this.framesHold = 5
+		this.framesMax = framesMax;
+		this.framesCurrent = 0;
+		this.framesElapsed = 0;
+		this.framesHold = 5;
+		this.offset = offset;
 	}
 
 	draw() {
 		c.drawImage(
 			this.image,
-            this.framesCurrent * (this.image.width / this.framesMax),
-            0,
-            this.image.width / this.framesMax,
-            this.image.height,
-			this.position.x,
-			this.position.y,
+			this.framesCurrent * (this.image.width / this.framesMax),
+			0,
+			this.image.width / this.framesMax,
+			this.image.height,
+			this.position.x - this.offset.x,
+			this.position.y - this.offset.y,
 			(this.image.width / this.framesMax) * this.scale,
 			this.image.height * this.scale
-		)
+		);
 	}
+
+    animateFrames() {
+        this.framesElapsed++;
+
+        if (this.framesElapsed % this.framesHold === 0) {
+			if (this.framesCurrent < this.framesMax - 1) {
+				this.framesCurrent++;
+			} else {
+				this.framesCurrent = 0;
+			}
+		}
+    }
 
 	update() {
 		this.draw();
-        this.framesElapsed++
-
-        if (this.framesElapsed % this.framesHold === 0) {
-            if (this.framesCurrent < this.framesMax - 1) {
-                this.framesCurrent++
-            } else {
-                this.framesCurrent = 0
-            }
-        }
 	}
 }
 
 // Set Fighter Class
-class Fighter {
-	constructor({ position, velocity, color = "red", offset }) {
-		this.position = position;
+class Fighter extends Sprite {
+	constructor({
+		position,
+		velocity,
+		color = "red",
+		imageSrc,
+		scale = 1,
+		framesMax = 1,
+        offset = {x: 0, y: 0}
+	}) {
+		super({
+			position,
+			imageSrc,
+			scale,
+			framesMax,
+            offset
+		});
+
 		this.velocity = velocity;
 		this.width = 50;
 		this.height = 150;
@@ -61,26 +86,15 @@ class Fighter {
 		this.color = color;
 		this.isAttacking;
 		this.health = 100;
-	}
-
-	draw() {
-		c.fillStyle = this.color;
-		c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-		// Attack box
-		if (this.isAttacking) {
-			c.fillStyle = "green";
-			c.fillRect(
-				this.attackBox.position.x,
-				this.attackBox.position.y,
-				this.attackBox.width,
-				this.attackBox.height
-			);
-		}
+		this.framesCurrent = 0;
+		this.framesElapsed = 0;
+		this.framesHold = 5;
 	}
 
 	update() {
 		this.draw();
+        this.animateFrames()
+        
 		this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
 		this.attackBox.position.y = this.position.y;
 
